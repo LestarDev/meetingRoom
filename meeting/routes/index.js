b23fec446387780b63bb29a7a396c75096b3080a
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-08-14 22:43:38 +0100

    add auth

--- meeting/routes/index.js
+++ meeting/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/meeting', async function(req, res, next){
+    const title = req.body.title;
+
+    const linkP = await axios.post(`${baseURL}/organizating/${orgId}/meeting`, {
+      title : title || "Meet",
+    },{
+      headers: {
+        'Authorization': `APIKEY ${apiKey}`
+      }
+    });
+    const meeting = resp.data.data.meeting;
+    const roomName = meeting.roomName;
+    const meetingId = meeting.id;
+    res.render('dashboard', { meetingId: meetingId, roomName });
 });
 
 module.exports = router;
