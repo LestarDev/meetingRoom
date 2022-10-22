b4c5e15b184f3ab2f1a9afdbd3233f4a32b77fb8
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-10-22 11:25:41 +0200

    add more change

--- game2/src/app/client/app.js
+++ game2/src/app/client/app.js
@@ -5,16 +5,17 @@ import MainPage from '../components/pages/main.js'
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send( 
+  res.send(
     MainPage(
       `Staty: ATK: ${Entity.player.ATK}, DEF: ${Entity.player.DEF}, HP: ${Entity.player.HP}, MOC: ${Entity.player.MOC}`,
       `Broń: Dodatkowe obr - ${Entity.player.Item.ATK}, Opis - ${Entity.player.Item.opis}`
     )    
   )
+  Entity.player.ATK=20;
 })
 
 app.get('/xxx', function (req, res) {
-  res.send("xxx")
+  res.send(`${Entity.player.ATK}`)
 })
 
 app.listen(3000)
